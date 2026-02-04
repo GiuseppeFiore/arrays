a547391cc1c6f9728a24ebc9b7e4058cea70709f
Giuseppe Fiore <giuseppe100891fiore@gmail.com>
2026-02-04 14:06:45 +0100

    correct arrays 01

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,31 +11,27 @@ const teachers = [
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
-console.log('Fourth teacher:', fourthTeacher);
+console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers[4] = 'Patrick';
-console.log('After replace:', teachers);
+console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
-console.log('Teachers:', teachers);
-console.log('Last teacher:', lastTeacher);
-
+console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
-console.log('Teachers:', teachers);
-console.log('First teacher:', firstTeacher);
-
+console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push('Vanessa');
-console.log('After adding Vanessa:', teachers);
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 teachers.unshift('Sarah');
-console.log('After adding Sarah:', teachers);
+console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
