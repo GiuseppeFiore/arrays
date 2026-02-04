4e3de3498176063e16caf06cdfb99cddc37d8c8b
Giuseppe Fiore <giuseppe100891fiore@gmail.com>
2026-02-04 15:42:28 +0100

    3 point arrays 02

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,6 +15,7 @@ const reversedTeachers = [];
 for (let i = teachers.length - 1; i >= 0; i--) {
   reversedTeachers[reversedTeachers.length] = teachers[i];
 }
+// OPTION SOLUTION reversedTeacher[teachers.length -1-i]] = teachers[i];
 
 console.log(reversedTeachers);
 
@@ -31,10 +32,22 @@ for (let i = 0; i < teachers.length; i++) {
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+// Utilizzo splice visot in mdn che modifica il contenuto delle array rimuovendo o sostituendo gli elementi esistenti
+// non posso usare ne shift ne pop perche rimuovono il primo e ultimo elemento della lista
+const edIndex = teachers.indexOf('Ed');
+
+if (edIndex !== -1) {
+  teachers.splice(edIndex, 1);
+}
+
+console.log(teachers);
+
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = null;
+
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
